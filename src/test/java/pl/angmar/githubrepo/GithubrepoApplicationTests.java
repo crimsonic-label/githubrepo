0ff9c4e60d1837df75f477233652a14695fd446f
Primoz.P <quiet.drops@gmail.com>
2019-04-19 14:44:11 +0200

    Spring project from Initializr

---
+++ src/test/java/pl/angmar/githubrepo/GithubrepoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.angmar.githubrepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubrepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
